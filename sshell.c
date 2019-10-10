6d087c3f2632172c25c36368852c5de7d9f19733
xu1998hz <wedxu@ucdavis.edu>
2019-10-09 21:43:26 -0700

    phase 0 done 3 warning though

--- sshell.c
+++ sshell.c
@@ -6,7 +6,15 @@ int main(int argc, char *argv[])
 	char *cmd = "/bin/date -u";
 	int retval;
 
-	retval = system(cmd);
+	int pid = fork();
+	if (pid == 0) {
+		/* Child process*/
+		retval = execl("/bin/sh", "sh", "-c", cmd, (char *) NULL);
+		perror("execl");
+		exit(1);
+	}
+	wait(NULL);
+
 	fprintf(stdout, "Return status value for '%s': %d\n", cmd, retval);
 
 	return EXIT_SUCCESS;
