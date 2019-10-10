576faa0b172a2e31f15510e05ed266667f559d6b
xu1998hz <wedxu@ucdavis.edu>
2019-10-10 16:29:46 -0700

    update on everything before phase 2, self test done

--- sshell.c
+++ sshell.c
@@ -4,21 +4,45 @@
 #include <sys/wait.h>
 #include <errno.h>
 #include <string.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[])
 {
-	char *cmd = "/bin/date -u";
-	int pid = fork();
-	int status = 0;
-	if (pid == 0) {
-		/* Child process*/
-		execl("/bin/sh", "sh", "-c", cmd, (char *) NULL);
-		exit(1);
-	} else{
+	while (true)
+	{
+		printf("sshell$ ");
+		// Maximum input line size is 512
+		size_t buffersize = 512;
+		char *cmd = (char *)malloc(buffersize * sizeof(char));
+		// Check malloc allocation success
+		if (cmd == NULL)
+		{
+			perror("Unable to allocate buffer");
+			exit(1);
+		}
+		// read line from user input currently assume just command
+		getline(&cmd, &buffersize, stdin);
+		int status = 0;
+		// removes new line character, mark the end by end of line character
+		if (cmd[strlen(cmd)-1] == '\n')
+		{
+    	cmd[strlen(cmd)-1] = '\0';
+    }
+		// exit the shell
+	  if (strcmp(cmd, "exit") == 0) break;
+
+		char *args[]={cmd, NULL};
+		int pid = fork();
+		if (pid == 0)
+		{
+			/* Child process*/
+			execvp(args[0], args);
+			exit(1);
+		}
 		wait(&status);
+
+		fprintf(stderr, "+ completed \'%s\': [%d]\n", cmd, status);
+		// release memory for my command
+		free(cmd);
 	}
-
-	fprintf(stderr, "+ completed \'%s\': [%d]\n", cmd, status);
-
-	return EXIT_SUCCESS;
 }
