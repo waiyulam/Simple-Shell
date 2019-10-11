bfd4e59857625f9676b9611ee79c56a8bca66625
xu1998hz <wedxu@ucdavis.edu>
2019-10-10 21:25:25 -0700

    update directory error handling and update command not found handling

--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 sshell : sshell.o
-		gcc -o sshell sshell.o 
+		gcc -o sshell sshell.o
 sshell.o : sshell.c
-		gcc -c -Wall -Werror sshell.c
+		gcc -c -Wall -Werror -g sshell.c
 clean :
 		rm -f sshell sshell.o
--- sshell.c
+++ sshell.c
@@ -5,6 +5,14 @@
 #include <errno.h>
 #include <string.h>
 #include <stdbool.h>
+#include <dirent.h>
+#include <errno.h>
+
+// currently assume maximum command line argument is 16
+#define Max_ARG 16
+extern int errno;
+
+int parseCmd(char* cmd, char** cmdArgs);
 
 int main(int argc, char *argv[])
 {
@@ -14,10 +22,15 @@ int main(int argc, char *argv[])
 		// Maximum input line size is 512
 		size_t buffersize = 512;
 		char *cmd = (char *)malloc(buffersize * sizeof(char));
+		char *cmdArgs[Max_ARG];
+		for (int i = 0; i < Max_ARG; i++)
+		{
+			cmdArgs[i] = (char *)malloc(buffersize * sizeof(char));
+		}
 		// Check malloc allocation success
 		if (cmd == NULL)
 		{
-			perror("malloc fails to allocate memory");
+			perror("malloc fails to alloscate memory");
 			exit(1);
 		}
 		// read line from user input currently assume just command
@@ -28,17 +41,24 @@ int main(int argc, char *argv[])
 		{
     	cmd[strlen(cmd)-1] = '\0';
     }
+
 		// exit the shell
 	  if (strcmp(cmd, "exit") == 0) break;
 
-		char *args[]={cmd, NULL};
+		int argSize = parseCmd(cmd, cmdArgs);
+		// Try to open a directory for cd command
+		DIR* dir = opendir(cmdArgs[1]);
+		// error handling type setting
+
+		/*printf("PATH : %s\n", getenv("PATH"));*/
+		char *args[] = {cmdArgs[0], NULL};
+
 		int pid = fork();
-		if (pid == 0)
-		{
+		if (pid == 0){
 			/* Child process, use execvp to execute command on env variable*/
-			execvp(args[0], args);
+			execvp(cmdArgs[0], args);
+			fprintf(stderr, "Error: command not found\n");
 			exit(1);
-			perror("execvp is not successfully executed");
 		} else if (pid == -1) {
 			/*fork error printing*/
 			exit(1);
@@ -48,8 +68,49 @@ int main(int argc, char *argv[])
 			wait(&status);
 		}
 
-		fprintf(stderr, "+ completed \'%s\': [%d]\n", cmd, status);
+		if (strcmp(cmdArgs[0], "cd")==0)
+		{
+			if (dir) {
+				closedir(dir);
+			} else if (ENOENT == errno) {
+				printf("Error: no such directory\n");
+			} else {
+				exit(1);
+				perror("some other system errors\n");
+			}
+		}
+
+		// Error handling printing option for cd
+		fprintf(stderr, "+ completed \'");
+		for (int i = 0; i<argSize; i++)
+		{	if (i < argSize-1){
+			fprintf(stderr, "%s ", cmdArgs[i]);
+			} else{
+			fprintf(stderr, "%s", cmdArgs[i]);
+			}
+		}
+
+		fprintf(stderr, "\' [%d]\n", WEXITSTATUS(status));
 		// release memory for my command
 		free(cmd);
 	}
+}
+
+int parseCmd(char* cmd, char** cmdArgs)
+{
+		char s[2] = " ";
+		cmdArgs[0] = strtok(cmd, s);
+		int count = 1;
+		char* token;
+		while (true)
+		{
+			token = strtok(NULL, s);
+			if (token != NULL){
+				cmdArgs[count] = token;
+			} else{
+				break;
+			}
+			count++;
+		}
+		return count;
 }
